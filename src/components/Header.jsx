9bdfe4dc0e830c3c8d737adbe198cabb3d18efee
AjithParthi <ajithkumar.p@nibavlifts.com>
2024-08-30 15:11:46 +0530

    Committed by Ajith

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function(){
     return(
         <header id="header">
-            <h1>React Investment Calculator</h1>
+            <h1>Ajith Investment Calculator</h1>
             <img src='./investment-calculator-logo.png' alt='Investment Calculator Logo' />
         </header>
     )
